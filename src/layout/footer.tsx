0eba7a14c32ffcc4825bc1863a61bfb9d3cf818e
kipiiler <hoanghohohaha@gmail.com>
2025-09-06 22:00:10 -0700

    update footer

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -33,30 +33,43 @@ const DefaultFooter = () => {
   };
 
   return (
-    <footer className="w-full bg-[black] border-t border-[#444] text-white font-mono tracking-widest uppercase">
-      <div className="container mx-auto py-10 block lg:flex justify-between">
-        <div className="text-sm pl-4 w-full md:w-auto">
-            <div className="flex flex-col items-start">
-            <div>
-              <span className="text-[#559CF8]">[SYSTEM::COPYRIGHT]</span> <a target="_blank" href="https://nousresearch.com/" className="underline">Nous Research</a>
-            </div>
-            <div>
-              <span className="text-[#559CF8]">[SYSTEM::CODE]</span> <a target="_blank" href="https://github.com/NousResearch/huskyholdem-bench" className="underline">GITHUB</a>
-            </div>
-            <div>
+    <>
+      <footer className="w-full bg-[black] border-t border-[#444] text-white font-mono tracking-widest uppercase">
+        <div className="container mx-auto py-10 flex flex-col lg:flex-row justify-between items-center lg:items-start">
+          <div className="text-sm pl-4 w-full lg:w-auto mb-4 lg:mb-0">
+              <div className="flex flex-col items-start">
+              <div>
+                <span className="text-[#559CF8]">[SYSTEM::COPYRIGHT]</span> <a target="_blank" href="https://nousresearch.com/" className="underline">Nous Research</a>
+              </div>
+              <div>
+                <span className="text-[#559CF8]">[SYSTEM::CODE]</span> <a target="_blank" href="https://github.com/NousResearch/huskyholdem-bench" className="underline">GITHUB</a>
+              </div>
+              <div>
               <span className="text-[#559CF8]">[SYSTEM::COLLECT_DATASET]</span> <a target="_blank" href="https://huggingface.co/collections/NousResearch/huskybench-68bc8aec53b626cd7a44f189" className="underline">HUGGINGFACE</a>
             </div>
-            </div>
-        </div>
-        <div className="text-sm pl-4 w-full md:w-auto">
-            <div className="flex flex-col items-start">
-            <div>
-              <span className="text-[#FFFFFF]">[SYS::CRED]</span>
-              <span className="text-[#559CF8]"> Built by [<a href="https://x.com/bha_ku21" target="_blank" className="text-[#559CF8] underline">Bhavesh</a>]</span>
-            </div>
-            <div>
-              <span className="text-[#FFFFFF]">[SYS::CRED]</span>
-              <span className="text-[#559CF8]"> Built by [<a href="https://www.kipiiler.me" target="_blank" className="text-[#559CF8] underline">KIPIILER</a>]</span>
+              </div>
+          </div>
+
+          {/* Center CITE button */}
+          <div className="flex items-center justify-center mb-4 lg:mb-0">
+            <button 
+              onClick={handleCiteClick}
+              className="text-[#559CF8] border border-[#559CF8] px-4 py-2 hover:bg-[#559CF8] hover:text-black transition-colors text-lg font-bold"
+            >
+              CITE
+            </button>
+          </div>
+
+          <div className="text-sm pl-4 w-full lg:w-auto">
+              <div className="flex flex-col items-start">
+              <div>
+                <span className="text-[#FFFFFF]">[SYS::CRED]</span>
+                <span className="text-[#559CF8]"> Built by [<a href="https://x.com/bha_ku21" target="_blank" className="text-[#559CF8] underline">Bhavesh</a>]</span>
+              </div>
+              <div>
+                <span className="text-[#FFFFFF]">[SYS::CRED]</span>
+                <span className="text-[#559CF8]"> Built by [<a href="https://www.kipiiler.me" target="_blank" className="text-[#559CF8] underline">KIPIILER</a>]</span>
+              </div>
             </div>
           </div>
         </div>
